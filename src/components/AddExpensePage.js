83425c385de6a33c3a1c3132b8722a308762f202
chrismcgale <mcga6260@mylaurier.ca>
2021-08-05 08:38:43 -0400

    Expense App

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const AddExpensePage = () => (
+    <div>
+        From Add component
+    </div>
+);
+
+export default AddExpensePage;
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const EditExpensePage = (props) => {
+    con
+    return (
+        <div>
+            From Edit component
+        </div>
+    );
+};
+
+export default EditExpensePage
